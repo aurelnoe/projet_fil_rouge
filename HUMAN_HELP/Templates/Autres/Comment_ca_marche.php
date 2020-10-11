4dee54e9b870b89c613db4ce55297303eb96e1ae
aurelien montaye <aurel5994@gmail.com>
2020-10-11 18:49:32 +0200

    Ajout du dossier HUMAN HELP

---
+++ HUMAN_HELP/Templates/Autres/Comment_ca_marche.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Comment ca marche</title>
+</head>
+<body>
+
+<?php include("../Bases/navbar.php") ?>
+    
+</body>
+</html>
---
+++ HUMAN_HELP/Templates/Bases/navbar.php
@@ -0,0 +1,67 @@
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+
+  <div class="collapse navbar-collapse" id="navbarsExample03">
+    <ul class="navbar-nav mr-auto">
+    <li class="nav-item">
+        <a class="nav-link" href="../Bases/accueil.php">Accueil</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../Security/form_connexion.php">Connexion</a>
+      </li> 
+      <li class="nav-item">
+        <a class="nav-link" href="../Security/form_inscription.php">Inscription</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../Contact/form_contact.php">Contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../Autres/Comment_ca_marche.php">Comment ca marche?</a>
+      </li>
+
+      <li class="nav-item">
+        <div class="dropdown">
+          <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Blog
+          </a>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <a class="dropdown-item" href="../Blog/liste_article.php">Liste des articles</a>
+            <a class="dropdown-item" href="../Blog/details_article.php">détails article</a>
+            <a class="dropdown-item" href="../Blog/form_ajout_article.php">Ajouter un article</a>
+            <a class="dropdown-item" href="../Blog/form_modif_article.php">Modifier un article</a>
+          </div>
+        </div>   
+      </li>
+
+      <li class="nav-item">
+        <div class="dropdown">
+          <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Compte
+          </a>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <a class="dropdown-item" href="../Compte_personnel/details_compte.php">Détails compte</a>
+            <a class="dropdown-item" href="../Compte_personnel/form_modif_compte.php">Modification compte</a>
+          </div>
+        </div>   
+      </li>
+
+      <li class="nav-item">
+        <div class="dropdown">
+          <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Missions
+          </a>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <a class="dropdown-item" href="../Missions/liste_mission.php">Liste des missions</a>
+            <a class="dropdown-item" href="../Missions/form_ajout_mission.php">ajout mission</a>
+            <a class="dropdown-item" href="../Missions/form_inscription_mission.php">participer à une mission</a>
+            <a class="dropdown-item" href="../Missions/details_mission.php">détails mission</a>
+            <a class="dropdown-item" href="../Missions/form_modif_mission.php">Modifier une mission</a>
+          </div>
+        </div>   
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Deconnexion</a>
+      </li>   
+    </ul>
+  </div>
+</nav>
