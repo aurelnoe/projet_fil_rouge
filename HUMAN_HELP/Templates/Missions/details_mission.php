1e0ee3b320bb0711428ac1eb89e7b88e20d956ba
aurelnoe <52176425+aurelnoe@users.noreply.github.com>
2020-10-15 16:31:47 +0200

    détails mission

--- HUMAN_HELP/Templates/Missions/details_mission.php
+++ HUMAN_HELP/Templates/Missions/details_mission.php
@@ -1,3 +1,85 @@
+<head>
+    <title>Détails de la mission</title>
+</head>
 <div class="container">
-    
+
+    <h2 class="text-center my-2 pb-3 pt-3">Détails de la mission</h2>
+
+    <hr class="mb-4 mt-2">
+    <!-- section image details -->
+    <div class="row h-25">
+        <img class="col-10 col-md-5 border m-auto imageDetailsMission rounded" width="100" height="360"/>
+        <div class="col-10 col-md-5">
+            <ul class="liDetailsMission">
+                <li>Titre de la mission : Développement numérique</li>
+                <li class="pt-2">Type d'activité : Informatique</li>
+                <li>Formation : à distance</li>
+                <li>Pays : Ghana ( Afrique centrale )</li>
+                <li>Organisateur : Association Développement Numérique d'Accra</li>
+                <li>Durée de la formation : 60 jours</li>
+                <li>Date début : 26 Mai 2021</li>
+            </ul>
+        </div>
+    </div>
+
+    <hr class="mb-4 mt-4">
+    <!-- description -->
+    <h2 class="mb-4 mt-4">Description :</h2>
+
+    <div class="mb-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga quae, a, ducimus error aut eos odio porro quam repellendus nisi, laborum corporis doloremque deserunt unde perferendis velit. Molestiae, rem fugit?
+        Adipisci inventore ad odit vitae suscipit voluptates! Libero nisi cum odio necessitatibus eos fuga ex pariatur, nesciunt illo, nam quos. Error tempora nihil similique ab consectetur optio laboriosam blanditiis sequi.
+        Quas tempora optio aliquid iusto facilis et rerum. Aperiam animi expedita, doloremque cumque id laborum eligendi maxime minima eaque quisquam sunt recusandae fugiat omnis reiciendis, vero, aliquam aut necessitatibus dolorem!
+    </div>
+    <div class="mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam harum nostrum suscipit quas doloribus exercitationem ipsum id alias laudantium illo cupiditate, voluptatibus perspiciatis similique labore at totam quos accusamus consequuntur.
+        Harum, sit eum. Quibusdam nulla voluptatum voluptatem quo minus sunt quod natus tempora mollitia saepe ea eaque a alias atque ullam maiores obcaecati magnam, veniam ad suscipit autem temporibus dicta.
+        Vitae labore ratione quia ab ipsam impedit sint reprehenderit fugiat dolorum at aliquid, neque illo voluptates alias ullam aliquam, quam, blanditiis cupiditate! Ullam odit magni, a natus quis nam omnis!
+    </div>
+    <div class="mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam harum nostrum suscipit quas doloribus exercitationem ipsum id alias laudantium illo cupiditate, voluptatibus perspiciatis similique labore at totam quos accusamus consequuntur.
+        Harum, sit eum. Quibusdam nulla voluptatum voluptatem quo minus sunt quod natus tempora mollitia saepe ea eaque a alias atque ullam maiores obcaecati magnam, veniam ad suscipit autem temporibus dicta.
+        Vitae labore ratione quia ab ipsam impedit sint reprehenderit fugiat dolorum at aliquid, neque illo voluptates alias ullam aliquam, quam, blanditiis cupiditate! Ullam odit magni, a natus quis nam omnis!
+    </div>
+        
+    <hr class="mb-4 mt-4">
+
+    <!-- COMMENTAIRES -->
+    <div class="border mb-4 pb-4">
+        <h2 class="mb-4 mt-4">Commentaires :</h2>
+        <div class="row border billet">
+            <div class="col-4">
+                <strong>@Maurice</strong>, 38 ans, Montpellier : <br>
+                Le 27/10/2020 11:59
+            </div>
+            <div class="col-8">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, temporibus doloremque? 
+            </div>            
+        </div>
+        <div class="row border billet">
+            <div class="col-4">
+            <strong>@Bob</strong>, 72 ans, Toulouse : <br>
+                Le 01/11/2020 22:36
+            </div>
+            <div class="col-8">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed, temporibus doloremque? 
+            </div>            
+        </div>
+    </div>
+
+    <hr class="mb-4 mt-4">
+
+    <div class="row mb-4">
+        <div class="col-7 col-md-5">
+            <button class="btn btnGreen w-100">Comment y participer ?</button>
+        </div>
+        <div class="col-7 offset-md-2 col-md-5">
+            <button class="btn btnGreen w-100">contactez nous</button>
+        </div>
+    </div>
+    <div class="row mb-4">
+        <div class="col-7 col-md-3">
+            <button class="btn btn-primary w-100">Modifier</button>
+        </div>
+        <div class="col-7 offset-md-6 col-md-3">
+            <button class="btn btn-danger w-100">Supprimer</button>
+        </div>
+    </div>
 </div>
