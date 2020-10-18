bff9aebafd92be6f6fff798eb01cc06a10380974
AreusDZ <samir-mekhloufi@hotmail.fr>
2020-10-18 19:17:28 +0200

    base footer

---
+++ HUMAN_HELP/Templates/Bases/footer.php
@@ -0,0 +1,27 @@
+<!-- <footer>
+    <div class="row text-center">
+        <div class="col-md-4">
+        <ul>
+            <li>Pays d'action</li>
+            <li>Types d'activités</li>
+            <li><a href="/HUMAN_HELP/?q=Autres/comment_ca_marche">Comment ça marche</a></li>
+            <li><a href="/HUMAN_HELP/?q=FAQ/faq">FAQ</a></li>
+            <li>Blog</li>
+            <li><a href="/HUMAN_HELP/?q=Security/form_inscription">Inscription</a></li>
+            <li><a href="/HUMAN_HELP/?q=Security/form_connexion">Connexion</a></li>
+        </ul>
+        </div>
+        <div class="col-md-4">
+        <ul>
+            <li>Informations générales</li>
+            <li>Politiques de confidentialités</li>
+            <li>Cookies</li>
+            <li>Conditions générales</li>
+        </ul>
+        </div>
+        <div class="col-md-4">
+        <button class="btn btnGreen btn-lg btn-block mb-5" type="submit">S'inscrire à la Newsletter</button>
+        <button class="btn btnGreen btn-lg btn-block mb-5" type="submit">Contact</button>
+        </div>
+    </div>
+</footer> -->
