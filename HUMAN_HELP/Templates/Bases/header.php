bd57bac6bb23136697e64b517f8f95cfd8ddef50
AreusDZ <samir-mekhloufi@hotmail.fr>
2020-10-18 18:10:34 +0200

    form contact, faq et header

---
+++ HUMAN_HELP/Templates/Bases/header.php
@@ -0,0 +1,55 @@
+<div class="container-fluid">
+<div class="row">
+<img class="logo"
+     href="HUMAN_HELP/?q=images/logo2.png"
+     alt="Logo">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Projets <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="/HUMAN_HELP/?q=missions/liste_missions" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Pays d'action
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Maroc</a>
+            <a class="dropdown-item" href="#">Somalie</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Types d'activités
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Enseignements</a>
+              <a class="dropdown-item" href="#">Médecine</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="/HUMAN_HELP/?q=Autres/comment_ca_marche">Comment ça marche</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="/HUMAN_HELP/?q=FAQ/faq" tabindex="-1" aria-disabled="true">FAQ</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/HUMAN_HELP/?q=Blog/liste_articles"  tabindex="-1" aria-disabled="true">
+              Blog
+            </a>
+          </li>
+      </ul>
+      <ul class="navbar-nav mr-auto">
+       <a class="nav-link" href="/HUMAN_HELP/?q=Security/form_inscription" tabindex="-1" aria-disabled="true">Inscription</a>
+      <a class="nav-link" href="/HUMAN_HELP/?q=Security/form_connexion" tabindex="-1" aria-disabled="true">Connexion</a>
+    </ul>
+    </div>
+  </nav>
+</div>
+</div>
