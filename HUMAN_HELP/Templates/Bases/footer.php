32c0c3a39447ec055bdd5ee10881b9289880bf4f
AreusDZ <samir-mekhloufi@hotmail.fr>
2020-10-19 20:12:23 +0200

    style footer, flush et placement

--- HUMAN_HELP/Templates/Bases/footer.php
+++ HUMAN_HELP/Templates/Bases/footer.php
@@ -1,26 +1,26 @@
 <footer>
     <div class="row text-center">
         <div class="col-md-4">
-        <ul class="list-footer">
-            <li><a href="#navbarPays">Pays d'action</a></li>
-            <li><a href="#navbarActivite">Types d'activités</a></li>
-            <li><a href="/HUMAN_HELP/?q=Autres/comment_ca_marche">Comment ça marche</a></li>
-            <li><a href="/HUMAN_HELP/?q=Security/form_inscription">Inscription</a></li>
-            <li><a href="/HUMAN_HELP/?q=Security/form_connexion">Connexion</a></li>
+        <ul class="list-footer list-group list-group-flush">
+            <li class="list-group-item"><a style="text-decoration:none" href="#navbarPays">Pays d'action</a></li>
+            <li class="list-group-item"><a style="text-decoration:none" href="#navbarActivite">Types d'activités</a></li>
+            <li class="list-group-item"><a style="text-decoration:none" href="/HUMAN_HELP/?q=Autres/comment_ca_marche">Comment ça marche</a></li>
+            <li class="list-group-item"><a style="text-decoration:none" href="/HUMAN_HELP/?q=Security/form_inscription">Inscription</a></li>
+            <li class="list-group-item"><a style="text-decoration:none" href="/HUMAN_HELP/?q=Security/form_connexion">Connexion</a></li>
         </ul>
         </div>
         <div class="col-md-4">
-        <ul class="list-footer">
-            <li>Politiques de confidentialités</li>
-            <li>Politique d'utilisation des cookies</li>
-            <li>Conditions générales</li> 
-            <li><a href="/HUMAN_HELP/?q=FAQ/faq">FAQ</a></li>
-            <li><a href="/HUMAN_HELP/?q=Blog/liste_article">Blog</a></li>
+        <ul class="list-footer list-group list-group-flush">
+            <li class="list-group-item">Politiques de confidentialités</li>
+            <li class="list-group-item">Politique d'utilisation des cookies</li>
+            <li class="list-group-item">Conditions générales</li> 
+            <li class="list-group-item"><a style="text-decoration:none" href="/HUMAN_HELP/?q=FAQ/faq">FAQ</a></li>
+            <li class="list-group-item"><a style="text-decoration:none" href="/HUMAN_HELP/?q=Blog/liste_article">Blog</a></li>
         </ul>
         </div>
-        <div class="col-md-4">
-        <button class="btn btnGreen btn-lg btn-block mb-5" type="submit">S'inscrire à la Newsletter</button>
-        <button class="btn btnGreen btn-lg btn-block mb-5" type="submit">Contact</button>
+        <div class="col-md-4 button-footer">
+        <button class="btn btnGreen btn-lg btn-block mb-5 button-footer1" type="submit">S'inscrire à la Newsletter</button>
+        <button class="btn btnGreen btn-lg btn-block mb-5 button-footer2" type="submit">Contact</button>
         </div>
     </div>
 </footer>
