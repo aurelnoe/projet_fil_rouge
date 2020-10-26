d7e5ba141aae60c77f8e039430d9ad06e12c102f
mohali59 <mohali59@icloud.com>
2020-10-26 20:50:54 +0100

    detail compte personnel

--- HUMAN_HELP/Templates/Compte_personnel/details_compte.php
+++ HUMAN_HELP/Templates/Compte_personnel/details_compte.php
@@ -2,5 +2,53 @@
     <title>Infos perso</title>
 </head>
 <div class="container">
+<div>
     <h2>Détails des informations personnelles</h2>
 </div>
+<hr class="my-4">
+
+<div class="row">
+  <div class="col-sm-9">
+    <h3>Mes coordonnées :</h3>
+        <div class="row">
+            <div class="col-auto mr-auto">
+                <p>Civilité :</p>
+                <p>Nom :</p>
+                <p>Prénom :</p>
+                <p>Date de naissance :</p>
+                <p>Adresse mail :</p>
+                <p>Numéro de téléphone :</p>
+            </div></br>
+            <div class="col-auto">
+                <p>Monsieur</p>
+                <p>Dupont</p>
+                <p>Jean-Marc</p>
+                <p>02/12/1980</p>
+                <p>jmdupont@mail.com</p>
+                <p>0607080990</p>
+            </div>
+        </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-sm-9">
+    <h3>Votre adresse :</h3>
+        <div class="row">
+            <div class="col-auto mr-auto">
+                <p>Nméro, libéllé de la voie :</p>
+                <p>Ville :</p>
+                <p>Code postale :</p>
+                <p>Complément d'adresse :</p>
+            </div>
+            <div class="col-auto">
+                <p>24, rue Lointaine</p>
+                <p>Paris</p>
+                <p>75000</p>
+                <p>-</p>
+
+            </div>
+        </div>
+  </div>
+</div>
+<hr class="my-4">
