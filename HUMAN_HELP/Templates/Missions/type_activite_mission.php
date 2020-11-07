409fa4b8a7a3a7eeba8e9f7f5851bc6ff0a71125
aurelien montaye <aurel5994@gmail.com>
2020-11-07 23:09:02 +0100

    Ajout pages liste_pays_mission et list_type_activite_mission

--- HUMAN_HELP/Templates/Missions/type_activite_mission.php
+++ HUMAN_HELP/Templates/Missions/type_activite_mission.php
@@ -1,3 +1,72 @@
-<div class="container">
-    <h2>Enseignement :</h2>
+<div class="container text-center ">
+
+    <h1 class="h1-select"><?php echo "Enseignement :"; ?></h1>
+    
+    <hr class="my-4 hrGreen ">
+
+    <div class="card-group my-4">
+        <div class="row mx-0 my-2">
+            <div class="card card-select col-12 col-md-5">
+                <div class="card-body">
+                    <img src="\HUMAN_HELP\images\enseignementThai.jpg" height="auto" width="100%" alt="">
+                    <div class="text-card-select">
+                        <h2 class="my-2">Description</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                            Dolor, officia! Sit unde iure eaque velit nisi, sapiente libero numquam.
+                        </p>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <a href="" class="btn btn-primary my-1 col-12 col-md-6">Détails de la mission</a>
+                </div>
+            </div>
+            <div class="card card-select col-12 col-md-5">
+                <div class="card-body">
+                    <img src="\HUMAN_HELP\images\enseignementThai.jpg" height="auto" width="100%" alt="">
+                    <div class="text-card-select">
+                        <h2 class="my-2">Description</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, 
+                            officia! amet consectetur adipisicing elit. Dolor, officia! Sit unde iure 
+                            eaque velit nisi, sapiente libero numquam.
+                        </p>
+                    </div>
+                </div>
+                <div class="card-footer mx-0">
+                    <a href="" class="btn btn-primary my-1 col-12 col-md-6">Détails de la mission</a>
+                </div>
+            </div>             
+        </div>
+
+        <div class="row mx-0 my-2">
+            <div class="card card-select col-12 col-md-5">
+                <div class="card-body">
+                    <img src="\HUMAN_HELP\images\enseignementThai.jpg" height="auto" width="100%" alt="">
+                    <div class="text-card-select">
+                        <h2 class="my-2">Description</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                            Dolor, officia! Sit unde iure eaque velit nisi, sapiente libero numquam.
+                        </p>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <a href="" class="btn btn-primary my-1 col-12 col-md-6">Détails de la mission</a>
+                </div>
+            </div>
+            <div class="card card-select col-12 col-md-5">
+                <div class="card-body">
+                    <img src="\HUMAN_HELP\images\enseignementThai.jpg" height="auto" width="100%" alt="">
+                    <div class="text-card-select">
+                        <h2 class="my-2">Description</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, 
+                            officia! amet consectetur adipisicing elit. Dolor, officia! Sit unde iure 
+                            eaque velit nisi, sapiente libero numquam.
+                        </p>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <a href="" class="btn btn-primary my-1 col-12 col-md-6">Détails de la mission</a>
+                </div>
+            </div>             
+        </div>
+    </div>
 </div>
