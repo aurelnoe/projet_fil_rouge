4713e67a0a057ed494cf7212029fb9e814828f29
mohali59 <mohali59@icloud.com>
2020-11-08 22:25:36 +0100

    page modification article

--- HUMAN_HELP/Templates/Blog/form_modif_article.php
+++ HUMAN_HELP/Templates/Blog/form_modif_article.php
@@ -1,3 +1,30 @@
-
+<h2>Formulaire modification d'article</h2>
 <div class="col-12 col-md-5 container pt-4 my-4 border rounded">
+        <div class="form-group">
+            <label for="usr">Titre de l'article :</label>
+            <input type="text" class="form-control" id="modifier_titre">
+        </div>
+        <div class="form-group">
+            <label for="comment">Phrase d'accroche :</label>
+            <input type="text" class="form-control" id="accroche">
+        </div>
+        <div class="form-group">
+            <label for="comment">Article :</label>
+            <textarea class="form-control" rows="5" id="article"></textarea>
+        </div></br>
+    
+        
+  <div class="form-group my-4">
+    <label for="exampleFormControlFile1">Example file input</label>
+    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+  </div>
+
+    
+        
+  <div class="row my-3">
+    <div class="col-md-5"><button type="button" class="btn btn-success float-left">Enrengistrer un brouillon</button></div>
+    <div class="col-md-3 offset-md-3"><button type="button" class="btn btn-success float-right">Réinitialiser</button></div>
+  </div>
 </div>
+
+        
