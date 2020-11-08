5c046fe28136043a28b8dd0ed07a763d0b9c3b39
mohali59 <mohali59@icloud.com>
2020-11-08 22:00:48 +0100

    page detail compte perso

--- HUMAN_HELP/Templates/Compte_personnel/details_compte.php
+++ HUMAN_HELP/Templates/Compte_personnel/details_compte.php
@@ -6,12 +6,11 @@
     <h2>Détails des informations personnelles</h2>
 </div>
 <hr class="my-4">
-
 <div class="row">
-  <div class="col-sm-9">
+  <div class="col-sm-12 m-5">
     <h3>Mes coordonnées :</h3>
         <div class="row">
-            <div class="col-auto mr-auto">
+            <div class="col-9 mr-auto">
                 <p>Civilité :</p>
                 <p>Nom :</p>
                 <p>Prénom :</p>
@@ -19,7 +18,7 @@
                 <p>Adresse mail :</p>
                 <p>Numéro de téléphone :</p>
             </div></br>
-            <div class="col-auto">
+            <div class="col-3">
                 <p>Monsieur</p>
                 <p>Dupont</p>
                 <p>Jean-Marc</p>
@@ -30,25 +29,24 @@
         </div>
   </div>
 </div>
-
 <div class="row">
-  <div class="col-sm-9">
-    <h3>Votre adresse :</h3>
-        <div class="row">
-            <div class="col-auto mr-auto">
-                <p>Nméro, libéllé de la voie :</p>
-                <p>Ville :</p>
-                <p>Code postale :</p>
-                <p>Complément d'adresse :</p>
-            </div>
-            <div class="col-auto">
-                <p>24, rue Lointaine</p>
-                <p>Paris</p>
-                <p>75000</p>
-                <p>-</p>
-
-            </div>
-        </div>
-  </div>
+    <div class="col-sm-12 m-5">
+        <h3>Votre adresse :</h3>
+                <div class="row">
+                        <div class="col-9 mr-auto">
+                            <p>Nméro, libéllé de la voie :</p>
+                            <p>Ville :</p>
+                            <p>Code postale :</p>
+                            <p>Complément d'adresse :</p>
+                        </div>
+                        <div class="col-3">
+                            <p>24, rue Lointaine</p>
+                            <p>Paris</p>
+                            <p>75000</p>
+                            <p>-</p>
+                        </div>
+                </div>
+    </div>
+</div>
 </div>
 <hr class="my-4">
