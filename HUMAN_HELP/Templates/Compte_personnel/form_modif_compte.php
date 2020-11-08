eb26ac788f381ac8482cca213be13b10cf91869a
mohali59 <mohali59@icloud.com>
2020-11-08 21:21:22 +0100

    page modification information personelles

--- HUMAN_HELP/Templates/Compte_personnel/form_modif_compte.php
+++ HUMAN_HELP/Templates/Compte_personnel/form_modif_compte.php
@@ -2,6 +2,67 @@
     <title>Formulaire informations personnelles</title>
 </head>
 
-<div class="container">
-    <h2>Formulaire de modification des informations personnelles</h2>
+
+
+<div>
+    <h2 class="text-center">modification des informations personnelles</h2>
 </div>
+<hr class="my-4">
+<div class="col-12 col-md-5 container pt-4 my-4 border rounded">
+    <form>
+          <div class="form-group">
+              <label for="formGroupExampleInput">Nom :</label>
+              <input type="text" class="form-control" placeholder="nom">
+          </div>
+          <div class="form-group">
+            <label for="formGroupExampleInput2">Prénom :</label>
+            <input type="text" class="form-control" placeholder="Prénom">
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Addresse Email :</label>
+            <input type="email" class="form-control"  placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted"></small>
+          </div>
+            <div class="form-group">
+              <label for="formGroupExampleInput2">Numéro de téléphone :</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="form-group">
+              <label for="formGroupExampleInput2">Date de naissance :</label>
+              <input type="text" class="form-control" placeholder="jj/mm/aaaa">
+            </div>
+          <div>
+          <h4>Adresse Postale :</h4>
+          </div>
+          
+          <div class="form-group row">
+              <label class="col-sm-5 col-form-label "><small>Num, Libellé voie :</small></label>
+              <div class="col-sm-7">
+                <input type="text" class="form-control">
+              </div>
+              <label class="col-sm-5 col-form-label"><small>Code Postale :</small></label>
+              <div class="col-sm-7">
+                <input type="text" class="form-control" id="colFormLabel">
+              </div>
+              <label class="col-sm-5 col-form-label"><small>Adresse Compl. :</small></label>
+              <div class="col-sm-7">
+                <input type="text" class="form-control">
+              </div>
+              <label class="col-sm-5 col-form-label"><small>Mot de passe :</small></label>
+              <div class="col-sm-7">
+                <input type="text" class="form-control" placeholder="********">
+              </div>
+
+
+
+
+
+
+              </div>
+              
+              
+              
+        </div>
+  </form>
+  
+    
