49711a652319a5a3b42c21dc7f6a61ac5141b314
AreusDZ <samir-mekhloufi@hotmail.fr>
2020-11-11 16:42:57 +0100

    création de la class TypeActivite

---
+++ HUMAN_HELP/Class/TypeActivite.php
@@ -0,0 +1,54 @@
+<?php
+
+
+class TypeActivite{
+
+    private $idTypeActivite;
+    private $typeActivite;
+
+    public function __toString(){
+        $this->idTypeActivite;
+        $this->typeActivite;
+    }
+    
+
+    /**
+     * Get the value of idTypeActivite
+     */ 
+    public function getIdTypeActivite():int
+    {
+        return $this->idTypeActivite;
+    }
+
+    /**
+     * Set the value of idTypeActivite
+     *
+     * @return  self
+     */ 
+    public function setIdTypeActivite(int $idTypeActivite):self
+    {
+        $this->idTypeActivite = $idTypeActivite;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of typeActivite
+     */ 
+    public function getTypeActivite():string
+    {
+        return $this->typeActivite;
+    }
+
+    /**
+     * Set the value of typeActivite
+     *
+     * @return  self
+     */ 
+    public function setTypeActivite(string $typeActivite):self
+    {
+        $this->typeActivite = $typeActivite;
+
+        return $this;
+    }
+}
