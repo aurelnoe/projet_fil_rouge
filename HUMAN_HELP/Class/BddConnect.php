f474abc72c5a023e1d0fc3779dce0d8366e66b26
AreusDZ <samir-mekhloufi@hotmail.fr>
2020-11-19 18:00:31 +0100

    Class BddConnect

---
+++ HUMAN_HELP/Class/BddConnect.php
@@ -0,0 +1,12 @@
+<?php 
+
+class BddConnect {
+
+    public function connexion(){
+        //$db = new mysqli('localhost','root','','human_helps');
+        $db = new PDO("mysql:host=localhost;dbname=human_helps",'root','');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $db;
+    }
+    
+}
