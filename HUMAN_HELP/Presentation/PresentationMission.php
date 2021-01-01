e3053d92c5581381fd92a30d84c490c3f4a385b9
aurelien montaye <aurel5994@gmail.com>
2021-01-01 18:41:24 +0100

    amélioration affichage formulaire participation mission

--- HUMAN_HELP/Presentation/PresentationMission.php
+++ HUMAN_HELP/Presentation/PresentationMission.php
@@ -530,7 +530,7 @@ function detailsMission($mission,string $typeFormation,$newPays=null,$newTypeAct
             <!-- description -->
             <h2 class="mb-4 mt-4">Description :</h2>
 
-            <div class="mb-3"><?php echo $mission->getDescriptionMission(); ?></div>
+            <div class="my-4"><?php echo $mission->getDescriptionMission(); ?></div>
                 
             <hr class="mb-4 mt-5">
 
@@ -802,43 +802,43 @@ function formParticipationMission($mission)
         <?php
         include("../../Templates/Bases/navbarDev.php");
         ?>
-        <div class="container col-12 col-md-6 pt-4 my-4 border rounded">
+        <div class="container col-12 col-md-6 formParticipationMission py-4 rounded">
 
-            <h1 class="my-3 text-center"><?php echo $mission->getTitreMission(); ?></h1>
+            <h1 class="my-3 text-center">Titre de la mission : <?php echo $mission->getTitreMission(); ?></h1>
 
             <form class="needs-validation p-3" action="../../Templates/Autres/comment_ca_marche?action=participe&idMission=<?php echo $mission->getIdMission();?>" method="POST" novalidate>
                 <h2 class="text-center my-2 pb-2">Inscrivez vous</h2>
 
-                <hr class="mb-4 mt-2">
+                <hr class="mb-4 mt-2 hrGreen">
                 
-                <div class="d-block mb-3 form-group">
+                <div class="d-block my-4 form-group">
                     <label>Civilité</label>
                     <div class="row">
-                        <div class="custom-control custom-radio mx-4">
+                        <div class="custom-control custom-radio col-5 mx-4">
                             <input name="civilite" id="monsieur" type="radio" class="custom-control-input">
                             <label for="monsieur" class="custom-control-label">Monsieur</label>
                         </div>
-                        <div class="custom-control custom-radio mx-2">
+                        <div class="custom-control custom-radio col-5 mx-2">
                             <input name="civilite" id="madame" type="radio" class="custom-control-input">
                             <label for="madame" class="custom-control-label">Madame</label>
                         </div>
                     </div>  
                 </div>
-                <div class="mb-3 form-group">
+                <div class="my-4 form-group">
                     <label for="mail">Adresse mail</label>
                     <input name="mail" type="mail" class="form-control" placeholder="mail@mail.com" value="" required>
                     <div class="invalid-feedback">
                         Ce champ est requis.
                     </div>
                 </div>
-                <div class="mb-3 form-group date">
+                <div class="my-4 form-group date">
                         <label for="dateNaissance">Date de naissance</label>
                         <input name="dateNaissance" type="date" data-provide="datepicker" class="form-control" placeholder="jj/mm/aaaa" required>
                     <div class="invalid-feedback">
                         Ce champ est requis.
                     </div>
                 </div>
-                <div class="mb-3 form-group">
+                <div class="my-4 form-group">
                     <label for="message">Message</label>
                     <input name="message" type="textarea" class="form-control messageMission" placeholder="Ajouter un message"></input>
                     <div class="invalid-feedback">
@@ -846,7 +846,7 @@ function formParticipationMission($mission)
                     </div>
                 </div>
                 
-                <hr class="mb-4 mt-4">
+                <hr class="my-4 hrGreen">
                 
                 <button type="submit" class="btn btnGreen btn-lg btn-block mb-5">Envoyer</button>
                 <a href="/HUMAN_HELP/Templates/Autres/Comment_ca_marche.php?idMission=<?php echo $mission->getIdMission();?>" class="btn btn-primary w-100">Retour aux informations</a>        </form>
