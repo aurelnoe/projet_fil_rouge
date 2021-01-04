72dd28e65b038f0f96aea4bec7230de98060aeb4
AreusDZ <samir-mekhloufi@hotmail.fr>
2021-01-04 09:28:29 +0100

    correction faq

--- HUMAN_HELP/Presentation/PresentationBlog.php
+++ HUMAN_HELP/Presentation/PresentationBlog.php
@@ -1,29 +1,16 @@
 <?php
 
-function head()
-{
-?>
-    <?php include("../../head.php"); ?>
-    <script src="../Assets/jquery-3.5.1.min.js"></script>
-    <script src="Assets/script.js"></script>
-    <link rel="stylesheet" type="text/css" href="../../Assets/style.css">
-    <link rel="stylesheet" type="text/css" href="../../Assets/style.css">
-    <link rel="stylesheet" type="text/css" href="../../Assets/styleAurel.css">
-    <link rel="stylesheet" type="text/css" href="../../Assets/styleSamir.css">
-<?php
-}
-
 function afficher()
 {
-?>
+    ?>   
     <!DOCTYPE html>
     <html lang="en">
-
-    <head>
-        <?php echo head(); ?>
-    </head>
-<?php
+        <head>
+        <?php include("../../head.php"); ?>
+        </head>   
+    <?php
 }
+
 
 function formulaireArticle(string $title, $article, string $titleBtn, string $action, int $idArticle = null)
 {
