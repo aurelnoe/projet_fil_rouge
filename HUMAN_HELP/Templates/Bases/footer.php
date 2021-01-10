505aff0a87d9d28aa55d1967ae75b2463d3739b2
AreusDZ <samir-mekhloufi@hotmail.fr>
2021-01-10 17:42:23 +0100

    maj des liens du footer avec session pour inscription/deconnexion

--- HUMAN_HELP/Templates/Bases/footer.php
+++ HUMAN_HELP/Templates/Bases/footer.php
@@ -4,9 +4,19 @@
         <ul class="list-footer list-group list-group-flush">
             <li class="list-group-item"><a style="text-decoration:none" href="#navbarSupportedContent">Pays d'action</a></li>
             <li class="list-group-item"><a style="text-decoration:none" href="#navbarSupportedContent">Types d'activités</a></li>
-            <li class="list-group-item"><a style="text-decoration:none" href="/HUMAN_HELP/Templates/Autres/comment_ca_marche.php">Comment ça marche</a></li>
-            <li class="list-group-item"><a style="text-decoration:none" href="/HUMAN_HELP/Templates/Utilisateur/form_inscription.php">Inscription</a></li>
-            <li class="list-group-item"><a style="text-decoration:none" href="/HUMAN_HELP/Templates/Utilisateur/form_connexion.php">Connexion</a></li>
+            <li class="list-group-item"><a style="text-decoration:none" href="/HUMAN_HELP/Controller/CommentCaMarcheController/CommentCaMarcheController.php">Comment ça marche</a></li>
+            <?php
+				if (empty($_SESSION)) {
+				?>
+            <li class="list-group-item"><a style="text-decoration:none" href="/HUMAN_HELP/Controller/UtilisateurController/FormulairesUtilisateurController.php?action=formAjout">Inscription</a></li>
+            <?php }else{ ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="/HUMAN_HELP/Controller/AccueilController.php?action=deconnection" tabindex="-1" aria-disabled="true">Deconnexion</a>
+                </li>  
+            <?php } ?>
+            <li class="nav-item">
+                    <a class="nav-link" href="/HUMAN_HELP/Controller/MissionsController/listeMissionController.php" tabindex="-1" aria-disabled="true">Projets</a>
+            </li> 
         </ul>
         <!-- changer les liens ancres pour les mettres sur la banière -->
         </div>
@@ -15,8 +25,8 @@
             <li class="list-group-item">Politiques de confidentialités</li>
             <li class="list-group-item">Politique d'utilisation des cookies</li>
             <li class="list-group-item">Conditions générales</li> 
-            <li class="list-group-item"><a style="text-decoration:none" href="/HUMAN_HELP/Templates/FAQ/faq.php">FAQ</a></li>
-            <li class="list-group-item"><a style="text-decoration:none" href="/HUMAN_HELP/Templates/Blog/liste_article.php">Blog</a></li>
+            <li class="list-group-item"><a style="text-decoration:none" href="/HUMAN_HELP/Controller/FaqController/faqController.php">FAQ</a></li>
+            <li class="list-group-item"><a style="text-decoration:none" href="/HUMAN_HELP/Controller/BlogController/listeBlogController.php">Blog</a></li>
         </ul>
         </div>
         <div class="col-xl-4 col-md-12 button-footer">
